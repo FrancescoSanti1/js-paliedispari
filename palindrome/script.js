a680c065ea3e405c19e52b3745d6d53ba636c35f
francesco <francescosanti91@gmail.com>
2021-10-29 15:25:15 +0200

    aggiunta passi logici prima del codice

--- palindrome/script.js
+++ palindrome/script.js
@@ -3,6 +3,15 @@
 // Creare una funzione per capire se la parola inserita è palindroma.
 // Dare l’output relativo.
 
+// Passi logici:
+// chiedo all'utente di inserire una parola
+// salvo la lunghezza della parola inserita dall'utente
+// dichiaro la variabile flag che mi serve per sapere se la parola è palindroma
+// scorro tutti i caratteri della parola, fino a metà (non ha senso andare oltre, perché a questo punto sono già stati confrontati tutti, mentre nel caso di una parola di lunghezza dispari, il carattere di mezzo non avrà nessuna lettera con cui essere confrontato)
+// se trovo due lettere diverse tra loro, modifico la variabile flag
+// finito il controllo, se la mia flag è rimasta invariata, la parola è palindroma
+// altrimenti non lo è
+
 // chiedo all'utente di inserire una parola
 const parola = prompt("Inserisci una parola per verificare se è palindroma");
 
@@ -10,9 +19,10 @@ const parola = prompt("Inserisci una parola per verificare se è palindroma");
 const lunghezza = parola.length;
 // console.log(lunghezza);
 
+// invoco la funzione
 parolaPalindroma(parola, lunghezza);
 
-
+// dichiaro la funzione
 function parolaPalindroma(parolaInserita, numCaratteri) {
 
     // dichiaro la variabile flag che mi serve per sapere se la parola è palindroma
